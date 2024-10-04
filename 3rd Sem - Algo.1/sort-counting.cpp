421be8c8e51390da2ab7139ce443efce89c7b53e
Saptarshi Roy <saptarshiroy39@gmail.com>
2024-10-04 19:34:24 +0530

    Some Minor Changes

--- 3rd Sem - Algo.1/sort-counting.cpp
+++ 3rd Sem - Algo.1/sort-counting.cpp
@@ -1,50 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int findMax(int arr[], int start, int end)
+int findMax(int A[], int size)
 {
-  if (start == end)
+  int k = A[0];
+  for (int i = 1; i < size; i++)
   {
-    return arr[start];
+    if (A[i] > k)
+    {
+      k = A[i];
+    }
   }
-
-  int mid = (start + end) / 2;
-  int left = findMax(arr, start, mid);
-  int right = findMax(arr, mid + 1, end);
-
-  if (left > right)
-  {
-    return left;
-  }
-  else
-  {
-    return right;
-  }
+  return k;  // Return the max value
 }
 
 void countingSort(int A[], int B[], int C[], int k, int size)
 {
+  // Initialize count array C
   for (int i = 0; i <= k; i++)
   {
     C[i] = 0;
   }
 
+  // Count the occurrences of each element
   for (int j = 0; j < size; j++)
   {
     C[A[j]]++;
   }
 
+  // Cumulative count
   for (int i = 1; i <= k; i++)
   {
     C[i] = C[i] + C[i - 1];
   }
 
+  // Place the elements in the sorted order
   for (int j = size - 1; j >= 0; j--)
   {
     B[C[A[j]] - 1] = A[j];
     C[A[j]]--;
   }
 
+  // Print sorted array
   for (int i = 0; i < size; i++)
   {
     cout << B[i] << " ";
@@ -65,8 +62,8 @@ int main()
     cin >> A[i];
   }
 
-  int k = findMax(A, 0, size - 1);
-  cout << "Max value : " << k << endl;
+  int k = findMax(A, size);
+  cout << "Max value: " << k << endl;
 
   int C[k + 1];
   int B[size];
